731dfab10ad5eff4a6b513a8040d826fd823997d
aneupane <achyutneupane2940@gmail.com>
2015-11-10 03:46:09 -0800

    load changes

--- public/js/controllers/donate/donate.js
+++ public/js/controllers/donate/donate.js
@@ -42,43 +42,38 @@ angular.module('donate')
 
             $scope.products
                 = [{
-                name: 'Azurite',
+                name: 'John Family',
                 description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
                 shine: 8,
-                price: 110.50,
+                price: "Cleveland",
                 rarity: 7,
                 color: '#CCC',
                 faces: 14,
                 images: [
-                    "images/admin.png",
-                    "images/contact.jpeg",
-                    "images/donate.jpeg"
+                    "images/john.jpg"
+
                 ]
             }, {
-                name: 'Bloodstone',
+                name: 'John Family',
                 description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
                 shine: 9,
-                price: 22.90,
+                price: "cleveland",
                 rarity: 6,
                 color: '#EEE',
                 faces: 12,
                 images: [
-                    "images/admin.png",
-                    "images/contact.jpeg",
-                    "images/donate.jpeg"
+                    "images/john.jpg"
                 ]
             }, {
-                name: 'Zircon',
+                name: 'John Family',
                 description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
                 shine: 70,
-                price: 1100,
+                price: "cleveland",
                 rarity: 2,
                 color: '#000',
                 faces: 6,
                 images: [
-                    "images/admin.png",
-                    "images/contact.jpeg",
-                    "images/donate.jpeg"
+                    "images/john.jpg"
                 ]
             }];
 
