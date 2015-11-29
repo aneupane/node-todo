184e86437e0e4f771aac1a679b67b55cbf01f2f1
aneupane <achyutneupane2940@gmail.com>
2015-11-29 01:28:42 -0800

    all commited

--- public/js/controllers/register/registerdonar.js
+++ public/js/controllers/register/registerdonar.js
@@ -1,13 +1,34 @@
 ﻿
 angular.module('donate')
-    .controller('RegisterDonarController',  ['$scope', '$location','FamilyService','UserAuthorizationService',
+    .controller('RegisterDonarController',  ['$http' , '$scope', '$location','FamilyService','UserAuthorizationService',
 
 
-        function ($scope , $location , FamilyService , UserAuthorizationService) {
+        function ($http ,$scope , $location , FamilyService , UserAuthorizationService) {
 
             var myForm={};
 
+
+
+
+
+
+
+
+
             $scope.submitDonar= function () {
+
+                var data = JSON.stringify({
+                    userName: $scope.myForm.username,
+                    passWord: $scope.myForm.password,
+                    role : 'donar'
+                });
+
+                $http.post("http://localhost:8090/ksu-capstone-project-app/rest/userservice/register", data)
+                    .success(function (data, status) {
+
+                    });
+
+
                 $location.path('/register/donar/conformation');
             }
 
